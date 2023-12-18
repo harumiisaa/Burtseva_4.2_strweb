747da4020527c59180b5035c1c95fba2e5c9269c
harumiisaa <burtsevakseniya03@mail.ru>
2023-12-18 18:37:02 +0300

    Initial commit

---
+++ src/main/java/org/company/dao/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package src.main.java.org.company.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public static Connection getConnection() {
+        Connection c = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/students", "root", "Uy235891!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException " + e);
+        } catch (SQLException e) {
+            System.out.println("SQLException " + e);
+        }
+        return c;
+    }
+}
+
